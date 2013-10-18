e7fea0f4899e18c52e17b13980c581154d1a83fc
Roben Kleene <robenk@gmail.com>
2013-10-18 11:43:02 -0400

    Adding keyValueObserver.m snippet

---
+++ Cocoa/keyValueObserver.m
@@ -0,0 +1,24 @@
+#define kNameKeyPath @"name"
+
+- (id)initWithDictionary:(MyClass *)myObject 
+{
+    self = [super init];
+    if (self) {
+		_myObject = myObject;
+		[myObject addObserver:self forKeyPath:kNameKeyPath options:NSKeyValueObservingOptionNew context:NULL];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+	[_myObject removeObserver:self forKeyPath:kNameKeyPath context:NULL];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context 
+{
+    if (object == _myObject && [keyPath isEqual:kNameKeyPath]) {
+        NSString *changedName = [change objectForKey:NSKeyValueChangeNewKey];
+        // do something with changedName
+    }
+}
