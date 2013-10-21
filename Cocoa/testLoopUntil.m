da057eb7f46956f9925b155ff9a7e0acff0439b1
Roben Kleene <robenk@gmail.com>
2013-10-20 20:56:55 -0400

    Async Testing

--- Cocoa/testLoopUntil.m
+++ Cocoa/testLoopUntil.m
@@ -1,10 +1,11 @@
 __block BOOL hasCalledBack = NO;
 
-dispatch_async(dispatch_get_main_queue(), ^{    
+dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
     hasCalledBack = YES;
 });
 
-NSDate *loopUntil = [NSDate dateWithTimeIntervalSinceNow:1];
+NSUInteger timeoutInSeconds = 1;
+NSDate *loopUntil = [NSDate dateWithTimeIntervalSinceNow:timeoutInSeconds];
 while (hasCalledBack == NO && [loopUntil timeIntervalSinceNow] > 0) {
     [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:loopUntil];
 }
