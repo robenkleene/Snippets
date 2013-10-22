99028769fd9ebbc3da8e0454b77296af880c9253
Roben Kleene <robenk@gmail.com>
2013-10-21 22:20:02 -0400

    BOOL

--- Cocoa/testLoopUntil.m
+++ Cocoa/testLoopUntil.m
@@ -6,6 +6,6 @@ dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
 
 NSUInteger timeoutInSeconds = 1;
 NSDate *loopUntil = [NSDate dateWithTimeIntervalSinceNow:timeoutInSeconds];
-while (hasCalledBack == NO && [loopUntil timeIntervalSinceNow] > 0) {
+while (!hasCalledBack && [loopUntil timeIntervalSinceNow] > 0) {
     [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:loopUntil];
 }
