ab5f77332ce0267ddbdff9cec30b9b17c6eaed04
Roben Kleene <robenk@gmail.com>
2015-03-20 17:13:49 -0400

    Improve getopts
    * Works multiple times in same shell process

--- Shell/getopts.sh
+++ Shell/getopts.sh
@@ -2,6 +2,7 @@ usage () {
 	echo "Usage: command [-a] [-b argument]"
 }
 
+OPTIND=1 # Multiple `getopts` calls fail otherwise
 while getopts ab:h option; do
 	case "$option" in
 	    a)  echo "-a found"
