75d1b16eaefd26039f1b168141f5b79534ed5d65
Roben Kleene <robenk@gmail.com>
2015-10-22 17:17:57 -0400

    Update try

--- Swift/TryCatch.swift
+++ Swift/TryCatch.swift
@@ -1,7 +1,12 @@
+enum MyErrorType: ErrorType {
+    case NoError
+}
+
 do {
     let str = try NSString(contentsOfFile: "Foo.bar",
                            encoding: NSUTF8StringEncoding)
-}
-catch let error as NSError {
-    print(error.localizedDescription)
+} catch let error as NSError {
+	throw error
+} catch {
+	throw MyErrorType.NoError
 }
