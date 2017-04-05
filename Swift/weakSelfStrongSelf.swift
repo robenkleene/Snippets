dcafdc53b98e02d877671bd620ce1c26f0922da3
Roben Kleene <robenk@gmail.com>
2017-04-05 17:49:35 -0400

    Clean up snippet

--- Swift/weakSelfStrongSelf.swift
+++ Swift/weakSelfStrongSelf.swift
@@ -1,10 +1,4 @@
 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { [weak self] in
-    if let strongSelf = self {
-		// Use `strongSelf` here
-    }
-}
-
-dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { [weak self] in
     guard let `self` = self else { return }
 	// Use `self` here
 }
