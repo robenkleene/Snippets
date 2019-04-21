a658dd2cc813d275eeb71e5fdbd907f95334f5e4
Roben Kleene <contact@robenkleene.com>
2019-04-21 16:23:28 -0400

    Add typed collections

--- Objective-C/nullability.m
+++ Objective-C/nullability.m
@@ -1,4 +1,8 @@
 extern NSString * __nonnull  const MyConstant;
 NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong, nullable) Plugin *selectedPlugin;
+@property NSArray<NSString *> *stringArray;
+@property NSSet<NSString *> *stringSet;
+@property NSDictionary<NSString *, NSString *> *stringDict;
 NS_ASSUME_NONNULL_END
+
