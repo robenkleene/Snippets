9d1dd3b0cc59367d3da151163ab4c453884be150
Roben Kleene <contact@robenkleene.com>
2019-06-23 17:35:36 -0400

    Update snippet

--- Swift/dispatch_after.swift
+++ Swift/dispatch_after.swift
@@ -1,4 +1,3 @@
-let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
-dispatch_after(delayTime, dispatch_get_main_queue()) {
+DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
     // Do it
 }
