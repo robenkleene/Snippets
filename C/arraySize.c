a418d565fb21aa07395d5dc35d7e4fad8c974147
Roben Kleene <contact@robenkleene.com>
2020-02-28 11:12:25 -0500

    Update array

--- C/arraySize.c
+++ C/arraySize.c
@@ -1,2 +1,4 @@
 int v[] = {11, 12, 203, 260, 600, 601, 700, 800, 900, 910, 1000, 1010, 1000};
 size_t n = sizeof(v) / sizeof(int);
+
+// Note that the size of a *string* can be calculated with `strlen(s)`
