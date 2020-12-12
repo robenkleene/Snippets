666b250de986472ce6031d01528f3ddbe7df3dcb
Roben Kleene <contact@robenkleene.com>
2020-12-12 06:04:24 -0500

    Add linked list

---
+++ Python/linkedlist.py
@@ -0,0 +1,23 @@
+class ListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return self.data
+
+    def chain_string(self):
+        cur = self
+        data = []
+        while cur is not None:
+            data.append(str(cur.data))
+            cur = cur.next
+        data.append("None")
+        return " -> ".join(data)
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        return self.head.chain_string()
---
+++ Swift/linkedlist.swift
@@ -0,0 +1,31 @@
+class Node<T: Equatable> {
+    var value: T? = nil
+    var next: Node? = nil
+}
+
+extension Node: CustomStringConvertible {
+    var description: String {
+        guard let value = value else {
+            return "nil"
+        }
+        return "\(value)"
+    }
+}
+
+class LinkedList<T: Equatable> {
+  var head = Node<T>()
+}
+
+extension LinkedList: CustomStringConvertible {
+    var description: String {
+        var values = [String]()
+        var cur: Node? = head
+        while cur != nil {
+            if let cur = cur {
+                values.append("\(cur)")
+            }
+            cur = cur?.next
+        }
+        return values.joined(separator: " -> ")
+    }
+}
