a930eebf2530f9003341d1493cdf355d951f8532
Roben Kleene <contact@robenkleene.com>
2020-12-19 06:09:59 -0500

    Update linked list

--- Swift/linkedList.swift
+++ Swift/linkedList.swift
@@ -1,25 +1,24 @@
-class ListNode<T> {
-    var value: T?
-    var next: ListNode?
+typealias ListNode = Node<Int>
+
+class Node<T> {
+    var value: T
+    var next: Node?
+
+    init(value: T) {
+        self.value = value
+    }
 }
 
-extension ListNode: CustomStringConvertible {
+extension Node: CustomStringConvertible {
     var description: String {
-        guard let value = value else {
-            return "nil"
-        }
         return "\(value)"
     }
 }
 
-class LinkedList<T> {
-  var head = ListNode<T>()
-}
-
-extension LinkedList: CustomStringConvertible {
-    var description: String {
+extension Node {
+    func chainString() -> String {
         var values = [String]()
-        var cur: ListNode? = head
+        var cur: Node? = self
         while cur != nil {
             if let cur = cur {
                 values.append("\(cur)")
@@ -28,4 +27,32 @@ extension LinkedList: CustomStringConvertible {
         }
         return values.joined(separator: " -> ")
     }
+}
+
+class LinkedList<T> {
+    var head: Node<T>?
+}
+
+extension LinkedList: CustomStringConvertible {
+    var description: String {
+        guard let head = head else {
+            return "nil"
+        }
+        return head.chainString()
+    }
+}
+
+func makeList<T>(from array: [T]) -> LinkedList<T> {
+    let llist = LinkedList<T>()
+    var current: Node<T>?
+    for item in array {
+        let node = Node<T>(value: item)
+        if let current = current {
+            current.next = node
+        } else {
+            llist.head = node
+        }
+        current = node
+    }
+    return llist
 }
